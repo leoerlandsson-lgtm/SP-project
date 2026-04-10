c494808144b7dec0bb98d91204552f54d43396e5
Leo Erlandsson <leo.erlandsson@learnet.se>
2026-04-10 11:14:19 +0200

    i remade the project and stringed some new pokemonj via attack HP etc

--- SP project/Program.cs
+++ SP project/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create two Pokémon to test the system
+            Pokemon pikachu = new Pokemon("Pikachu", 35, 10);
+            Pokemon bulbasaur = new Pokemon("Bulbasaur", 45, 8);
+
+            // Simple test output
+            Console.WriteLine($"{pikachu.Name} HP: {pikachu.HP}");
+            Console.WriteLine($"{bulbasaur.Name} HP: {bulbasaur.HP}");
+        }
+    }
+
+    // FIRST thing you need: a Pokémon class
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+
+        public Pokemon(string name, int hp, int attack)
+        {
+            Name = name;
+            HP = hp;
+            Attack = attack;
         }
     }
 }
