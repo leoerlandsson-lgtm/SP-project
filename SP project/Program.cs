6ff522447528d5ea153b63fb2a5ff50c13f41690
Leo Erlandsson <leo.erlandsson@learnet.se>
2026-05-18 10:59:13 +0200

    restarted my cod with extra stuff

--- SP project/Program.cs
+++ SP project/Program.cs
@@ -1,20 +1,55 @@
-﻿namespace SP_project
+﻿using System;
+
+namespace SP_project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Create two Pokémon to test the system
+            // Skapa två Pokémon att testa systemet med
             Pokemon pikachu = new Pokemon("Pikachu", 35, 10);
             Pokemon bulbasaur = new Pokemon("Bulbasaur", 45, 8);
 
-            // Simple test output
-            Console.WriteLine($"{pikachu.Name} HP: {pikachu.HP}");
-            Console.WriteLine($"{bulbasaur.Name} HP: {bulbasaur.HP}");
+            Console.WriteLine("Striden börjar! En vild Bulbasaur dyker upp!");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Tryck på ENTER för att gå vidare i striden...\n");
+            Console.ReadLine();
+
+            // Stridssloop: Fortsätt så länge BÅDA har HP kvar (över 0)
+            while (pikachu.HP > 0 && bulbasaur.HP > 0)
+            {
+                // 1. Pikachu attackerar först
+                pikachu.AttackTarget(bulbasaur);
+
+                // Kontrollera om Bulbasaur svimmade av attacken
+                if (bulbasaur.HP <= 0)
+                {
+                    Console.WriteLine($"\n{bulbasaur.Name} svimmade! {pikachu.Name} vann striden!");
+                    break; // Avbryt loopen direkt, spelet är slut
+                }
+
+                Console.ReadLine(); // Vänta på att spelaren trycker Enter
+
+                // 2. Bulbasaur kontrar om den fortfarande lever
+                bulbasaur.AttackTarget(pikachu);
+
+                // Kontrollera om Pikachu svimmade av kontringen
+                if (pikachu.HP <= 0)
+                {
+                    Console.WriteLine($"\n{pikachu.Name} svimmade! {bulbasaur.Name} vann striden!");
+                    break; // Avbryt loopen
+                }
+
+                Console.ReadLine(); // Vänta på Enter innan nästa runda börjar
+                Console.WriteLine("--- Nästa runda ---");
+            }
+
+            Console.WriteLine("\nStriden är slut! Tack för att du spelade.");
+            Console.ReadLine(); // Håller kvar konsolfönstret i slutet
         }
     }
 
-    // FIRST thing you need: a Pokémon class
+    // Din Pokémon-klass
     public class Pokemon
     {
         public string Name { get; set; }
@@ -28,23 +63,15 @@
             Attack = attack;
         }
 
-
         public void AttackTarget(Pokemon target)
         {
             Console.WriteLine($"{this.Name} attackerar {target.Name}!");
             target.HP -= this.Attack;
 
-            
+            // Om HP hamnar under 0, sätt det till 0 så det ser snyggare ut i texten
             if (target.HP < 0) target.HP = 0;
 
             Console.WriteLine($"{target.Name} tog {this.Attack} skada och har nu {target.HP} HP kvar.");
         }
-
-
-
-
-
-
-
     }
 }
