7443a1e6d7aab7407c489543aa1f3fb92cac4e9b
Leo Erlandsson <leo.erlandsson@learnet.se>
2026-05-22 10:14:21 +0200

    Jag ändrade koden samt tog bort det mesta

--- SP project/Program.cs
+++ SP project/Program.cs
@@ -7,19 +7,38 @@ namespace SP_project
         static void Main(string[] args)
         {
             
-            Pokemon pikachu = new Pokemon("Pikachu", 35, 10);
+            Pokemon pikachu = new Pokemon("Pikachu", 40, 10);
             Pokemon bulbasaur = new Pokemon("Bulbasaur", 45, 8);
 
             Console.WriteLine("Striden börjar! En vild Bulbasaur dyker upp!");
-            Console.WriteLine("------------------------------------------");
-            Console.WriteLine("Tryck på ENTER för att gå vidare i striden...\n");
-            Console.ReadLine();
+            Console.WriteLine("------------------------------------------\n");
 
             
             while (pikachu.HP > 0 && bulbasaur.HP > 0)
             {
-               
-                pikachu.AttackTarget(bulbasaur);
+                
+                Console.WriteLine($"--- {pikachu.Name}s tur ({pikachu.HP} HP kvar) ---");
+                Console.WriteLine("Vad vill du göra?");
+                Console.WriteLine("1. Attackera");
+                Console.WriteLine("2. Använd Heal Potion (+15 HP)");
+                Console.Write("Gör ditt val (1-2): ");
+
+                string val = Console.ReadLine();
+                Console.WriteLine();
+
+                if (val == "1")
+                {
+                    pikachu.AttackTarget(bulbasaur);
+                }
+                else if (val == "2")
+                {
+                    pikachu.Heal();
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Du tvekade och missade din chans att agera!");
+                }
 
                 
                 if (bulbasaur.HP <= 0)
@@ -28,7 +47,13 @@ namespace SP_project
                     break; 
                 }
 
-                Console.ReadLine(); 
+                
+                Console.WriteLine("\nTryck på valfri tangent för datorns tur...");
+                Console.ReadKey();
+                Console.Clear(); 
+
+               
+                Console.WriteLine($"--- {bulbasaur.Name}s tur ({bulbasaur.HP} HP kvar) ---");
 
                 
                 bulbasaur.AttackTarget(pikachu);
@@ -37,47 +62,64 @@ namespace SP_project
                 if (pikachu.HP <= 0)
                 {
                     Console.WriteLine($"\n{pikachu.Name} svimmade! {bulbasaur.Name} vann striden!");
-                    break; 
+                    break;
                 }
 
-                Console.ReadLine(); 
-                Console.WriteLine("--- Nästa runda ---");
+                Console.WriteLine("\nTryck på valfri tangent för nästa runda...");
+                Console.ReadKey();
+                Console.Clear();
             }
 
             Console.WriteLine("\nStriden är slut! Tack för att du spelade.");
-            Console.ReadLine(); 
+            Console.ReadLine();
         }
     }
 
-    
     public class Pokemon
     {
+       
         public string Name { get; set; }
         public int HP { get; set; }
+        public int MaxHP { get; set; } 
         public int Attack { get; set; }
 
+        
         public Pokemon(string name, int hp, int attack)
         {
             Name = name;
             HP = hp;
+            MaxHP = hp; 
             Attack = attack;
         }
 
+        
         public void AttackTarget(Pokemon target)
         {
-            Console.WriteLine($"{this.Name} attackerar" +
-                $"" +
-                $"" +
-                $"" +
-                $"" +
-                $"" +
-                $" {target.Name}!");
+            Console.WriteLine($"{this.Name} attackerar {target.Name}!");
             target.HP -= this.Attack;
 
             
-            if (target.HP < 0) target.HP = 0;
+            if (target.HP < 0)
+            {
+                target.HP = 0;
+            }
 
             Console.WriteLine($"{target.Name} tog {this.Attack} skada och har nu {target.HP} HP kvar.");
+        }
+
+       
+        public void Heal()
+        {
+            int healAmount = 15;
+            this.HP += healAmount;
+
+            
+            if (this.HP > this.MaxHP)
+            {
+                this.HP = this.MaxHP;
+            }
+
+            Console.WriteLine($"{this.Name} drack en Potion och helade sig till {this.HP} HP!");
         }
     }
 }
